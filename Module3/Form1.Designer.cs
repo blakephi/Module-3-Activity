8d23b52041bf9ba7df7f7455ee57c4125b15a276
Darren Phillips <dzp0067@auburn.edu>
2021-03-20 17:03:20 -0500

    Created all form tools (text box, label, and button)

--- Module3/Form1.Designer.cs
+++ Module3/Form1.Designer.cs
@@ -29,13 +29,57 @@ namespace Module3
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.ResultsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(231, 133);
+            this.InputBox.Multiline = true;
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(313, 104);
+            this.InputBox.TabIndex = 0;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(331, 402);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(108, 36);
+            this.CalculateButton.TabIndex = 1;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            // 
+            // ResultsLabel
+            // 
+            this.ResultsLabel.AutoSize = true;
+            this.ResultsLabel.Location = new System.Drawing.Point(327, 240);
+            this.ResultsLabel.Name = "ResultsLabel";
+            this.ResultsLabel.Size = new System.Drawing.Size(71, 20);
+            this.ResultsLabel.TabIndex = 2;
+            this.ResultsLabel.Text = "Results: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.ResultsLabel);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.InputBox);
+            this.Name = "Form1";
+            this.Text = "Word Occurences Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Label ResultsLabel;
     }
 }
 
