c5ad1569b689bfa000487a32f946001d1950a0c1
Darren Phillips <dzp0067@auburn.edu>
2021-03-21 13:26:38 -0500

    Created button click event

--- Module3/Form1.cs
+++ Module3/Form1.cs
@@ -16,5 +16,10 @@ namespace Module3
         {
             InitializeComponent();
         }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
